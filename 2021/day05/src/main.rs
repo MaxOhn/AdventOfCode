a2fd3034533b45cb3312a2612d1adb3e3731e470
MaxOhn <ohn.m@hotmail.de>
2021-12-05 09:16:50 +0100

    day05 improvement

--- 2021/day05/src/main.rs
+++ 2021/day05/src/main.rs
@@ -23,67 +23,95 @@ fn run() -> Result<(), Box<dyn Error>> {
     let mut input = BufReader::new(file);
 
     let mut line = String::new();
-    let mut grid = vec![[0; 1000]; 1000];
+    let mut grid = vec![[0_u8; 1000]; 1000];
+
+    const BOTH_PARTS: u8 = 0b0001_0001;
+    const PART2: u8 = 0b0000_0001;
 
     while input.read_line(&mut line)? != 0 {
-        let mut split = line.split(" -> ");
-        let mut first = split.next().unwrap().split(',');
-        let mut second = split.next().unwrap().split(',');
+        let bytes = line.trim_end().as_bytes();
 
-        let x1: usize = first.next().unwrap().parse().unwrap();
-        let y1: usize = first.next().unwrap().parse().unwrap();
-        let x2: usize = second.next().unwrap().parse().unwrap();
-        let y2: usize = second.next().unwrap().trim_end().parse().unwrap();
+        let mut i = 0;
+        let x1 = parse_num(bytes, &mut i, b',');
+        i += 1;
+        let y1 = parse_num(bytes, &mut i, b' ');
+        i += 4;
+        let x2 = parse_num(bytes, &mut i, b',');
+        i += 1;
+
+        let mut y2 = (bytes[i] & 0x0F) as usize;
+        i += 1;
+
+        while i < bytes.len() {
+            y2 = y2 * 10 + (bytes[i] & 0x0F) as usize;
+            i += 1
+        }
 
         if x1 == x2 {
-            let min = y1.min(y2);
-            let max = y1.max(y2);
+            let range = if y1 < y2 { y1..=y2 } else { y2..=y1 };
 
-            for y in min..=max {
-                grid[y][x1] += 1;
+            for y in range {
+                grid[y][x1] += BOTH_PARTS;
             }
         } else if y1 == y2 {
-            let min = x1.min(x2);
-            let max = x1.max(x2);
+            let range = if x1 < x2 { x1..=x2 } else { x2..=x1 };
 
-            for x in min..=max {
-                grid[y1][x] += 1;
+            for x in range {
+                grid[y1][x] += BOTH_PARTS;
+            }
+        } else if (x1 > x2 && y1 > y2) || (x2 > x1 && y2 > y1) {
+            let (x_range, y_range) = if x1 < x2 {
+                (x1..=x2, y1..=y2)
+            } else {
+                (x2..=x1, y2..=y1)
+            };
+
+            for (x, y) in x_range.zip(y_range) {
+                grid[y][x] += PART2;
             }
         } else {
-            if (x1 > x2 && y1 > y2) || (x2 > x1 && y2 > y1) {
-                let x_min = x1.min(x2);
-                let x_max = x1.max(x2);
-                let y_min = y1.min(y2);
-                let y_max = y1.max(y2);
+            let x_range = if x1 < x2 { x1..=x2 } else { x2..=x1 };
+            let y_range = if y1 < y2 { y1..=y2 } else { y2..=y1 };
 
-                for (x, y) in (x_min..=x_max).zip(y_min..=y_max) {
-                    grid[y][x] += 1;
-                }
-            } else {
-                let x_min = x1.min(x2);
-                let x_max = x1.max(x2);
-                let y_min = y1.min(y2);
-                let y_max = y1.max(y2);
-
-                for (x, y) in (x_min..=x_max).rev().zip(y_min..=y_max) {
-                    grid[y][x] += 1;
-                }
+            for (x, y) in x_range.rev().zip(y_range) {
+                grid[y][x] += PART2;
             }
         }
 
         line.clear();
     }
 
-    let mut answer = 0;
+    let mut p1 = 0;
+    let mut p2 = 0;
 
     for row in grid {
         for count in row {
-            answer += (count > 1) as usize;
+            p1 += (count >= 0b0010_0000) as usize;
+            p2 += ((count & 0b0000_1111) > 1) as usize;
         }
     }
 
     let elapsed = start.elapsed();
-    println!("Answer: {} [{:?}]", answer, elapsed);
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+    println!("Elapsed: {:?}", elapsed);
+
+    assert_eq!(p1, 5632);
+    assert_eq!(p2, 22213);
 
     Ok(())
+}
+
+fn parse_num(bytes: &[u8], i: &mut usize, delim: u8) -> usize {
+    let mut n = (bytes[*i] & 0x0F) as usize;
+
+    loop {
+        *i += 1;
+
+        if bytes[*i] == delim {
+            return n;
+        }
+
+        n = n * 10 + (bytes[*i] & 0x0F) as usize;
+    }
 }
