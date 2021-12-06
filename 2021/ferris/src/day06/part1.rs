13669ae7ece8cadabea7243e10833cc3433f8eff
MaxOhn <ohn.m@hotmail.de>
2021-12-06 09:44:04 +0100

    unsafe-ified ferris part 1

--- 2021/ferris/src/day06/part1.rs
+++ 2021/ferris/src/day06/part1.rs
@@ -1,30 +1,29 @@
-#![feature(core_intrinsics)]
-
 pub fn run(input: &[u8]) -> i64 {
     let mut count = [0; 9];
-
     let mut n = 0;
 
     input.into_iter().for_each(|&byte| {
         if byte == b',' {
-            count[n] += 1;
+            unsafe { *count.get_unchecked_mut(n) += 1 };
             n = 0;
         } else {
             n = n * 10 + (byte & 0x0F) as usize;
         }
     });
 
-    count[n] += 1;
+    unsafe {
+        *count.get_unchecked_mut(n) += 1;
 
-    for _ in 0..80 {
-        let zero = count[0];
+        for _ in 0..80 {
+            let zero = *count.get_unchecked(0);
 
-        for n in 0..8 {
-            count[n] = count[n + 1]
+            for n in 0..8 {
+                *count.get_unchecked_mut(n) = *count.get_unchecked(n + 1);
+            }
+
+            *count.get_unchecked_mut(6) += zero;
+            *count.get_unchecked_mut(8) = zero;
         }
-
-        count[6] += zero;
-        count[8] = zero;
     }
 
     count.into_iter().sum()
