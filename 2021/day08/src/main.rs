8b87ba5ac79ed50f11a9bf3328af08dc48ab7a76
MaxOhn <ohn.m@hotmail.de>
2021-12-08 18:28:36 +0100

    first day 8 optimization

--- 2021/day08/src/main.rs
+++ 2021/day08/src/main.rs
@@ -29,85 +29,75 @@ fn run() -> Result<(), Box<dyn Error>> {
     let mut p2 = 0;
 
     while input.read_line(&mut line)? != 0 {
-        let mut wiring = [0; 7].map(|_| HashSet::new());
-
-        let mut split = line.trim_end().split(" | ");
-        let left = split.next().unwrap();
-        let right = split.next().unwrap();
+        let [mut a, mut b, mut c, mut d, mut e, mut f, mut g] =
+            [0; 7].map(|_| HashSet::with_capacity(10));
+        let (left, right) = line.trim_end().split_once(" | ").unwrap();
 
         for word in right.split(' ') {
             p1 += ([2, 3, 4, 7].contains(&word.len())) as usize;
         }
 
         let mut words: Vec<_> = left.split(' ').map(str::as_bytes).collect();
+        words.sort_unstable_by_key(|bytes| bytes.len());
 
-        let eight = words.iter().position(|word| word.len() == 7);
+        let mut words = words.into_iter();
 
-        if let Some(eight) = eight.map(|n| words.remove(n)) {
-            for &byte in eight {
-                for option in wiring.iter_mut() {
-                    option.insert(byte);
-                }
-            }
+        let [one, seven, four, _, _, _, six_1, six_2, six_3, eight] = [
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+            words.next().unwrap(),
+        ];
+
+        // Eight
+        for &byte in eight {
+            e.insert(byte);
+            g.insert(byte);
         }
 
-        let four = words.iter().position(|word| word.len() == 4);
-
-        if let Some(four) = four.map(|n| words.remove(n)) {
-            for i in [1, 2, 3, 5] {
-                wiring[i].clear();
-
-                for &byte in four {
-                    wiring[i].insert(byte);
-                }
-            }
-
-            wiring[0].retain(|byte| !four.contains(byte));
-            wiring[4].retain(|byte| !four.contains(byte));
-            wiring[6].retain(|byte| !four.contains(byte));
+        // Four
+        for &byte in four {
+            b.insert(byte);
+            d.insert(byte);
         }
 
-        let seven = words.iter().position(|word| word.len() == 3);
+        e.retain(|byte| !four.contains(byte));
+        g.retain(|byte| !four.contains(byte));
 
-        if let Some(seven) = seven.map(|n| words.remove(n)) {
-            for i in [0, 2, 5] {
-                wiring[i].clear();
-
-                for &byte in seven {
-                    wiring[i].insert(byte);
-                }
-            }
-
-            wiring[1].retain(|byte| !seven.contains(byte));
-            wiring[3].retain(|byte| !seven.contains(byte));
-            wiring[4].retain(|byte| !seven.contains(byte));
-            wiring[6].retain(|byte| !seven.contains(byte));
+        // Seven
+        for &byte in seven {
+            a.insert(byte);
         }
 
-        let one = words.iter().position(|word| word.len() == 2);
+        b.retain(|byte| !seven.contains(byte));
+        d.retain(|byte| !seven.contains(byte));
+        e.retain(|byte| !seven.contains(byte));
+        g.retain(|byte| !seven.contains(byte));
 
-        if let Some(one) = one.map(|n| words.remove(n)) {
-            for i in [2, 5] {
-                wiring[i].clear();
-
-                for &byte in one {
-                    wiring[i].insert(byte);
-                }
-            }
-
-            wiring[0].retain(|byte| !one.contains(byte));
-            wiring[1].retain(|byte| !one.contains(byte));
-            wiring[3].retain(|byte| !one.contains(byte));
-            wiring[4].retain(|byte| !one.contains(byte));
-            wiring[6].retain(|byte| !one.contains(byte));
+        // One
+        for &byte in one {
+            c.insert(byte);
+            f.insert(byte);
         }
+
+        a.retain(|byte| !one.contains(byte));
+        b.retain(|byte| !one.contains(byte));
+        d.retain(|byte| !one.contains(byte));
+        e.retain(|byte| !one.contains(byte));
+        g.retain(|byte| !one.contains(byte));
 
         // zero, six, nine
-        for &word_ in words.iter().filter(|w| w.len() == 6) {
-            let mut word: HashSet<_> = word_.into_iter().copied().collect();
+        for word_ in [six_1, six_2, six_3] {
+            let mut word: HashSet<_> = word_.iter().copied().collect();
 
-            for n in [0, 4, 5, 6] {
-                for byte in &wiring[n] {
+            for options in [&a, &e, &f, &g] {
+                for byte in options {
                     word.remove(byte);
                 }
             }
@@ -115,18 +105,18 @@ fn run() -> Result<(), Box<dyn Error>> {
             match word.len() {
                 // zero
                 1 => {
-                    word.retain(|byte| !wiring[2].contains(byte));
-                    word.retain(|byte| !wiring[5].contains(byte));
+                    word.retain(|byte| !c.contains(byte));
+                    word.retain(|byte| !f.contains(byte));
                     let remaining = word.into_iter().next().unwrap();
-                    wiring[1].retain(|&byte| byte == remaining);
-                    wiring[3].retain(|&byte| byte != remaining);
+                    b.retain(|&byte| byte == remaining);
+                    // d.retain(|&byte| byte != remaining);
                 }
                 // six or nine
                 2 => {
                     let mut set: HashSet<_> = word_.iter().copied().collect();
 
-                    for n in [0, 1, 2, 3, 5] {
-                        for byte in &wiring[n] {
+                    for options in [&a, &b, &c, &d, &f] {
+                        for byte in options {
                             set.remove(byte);
                         }
                     }
@@ -135,12 +125,12 @@ fn run() -> Result<(), Box<dyn Error>> {
                         // nine
                         let remaining = set.into_iter().next().unwrap();
 
-                        wiring[4].retain(|&byte| byte != remaining);
-                        wiring[6].retain(|&byte| byte == remaining);
+                        e.retain(|&byte| byte != remaining);
+                        // g.retain(|&byte| byte == remaining);
                     } else {
                         // six
-                        for &c in word_ {
-                            wiring[2].retain(|&byte| byte != c);
+                        for byte_ in word_ {
+                            c.retain(|byte| byte != byte_);
                         }
                     }
                 }
@@ -148,30 +138,31 @@ fn run() -> Result<(), Box<dyn Error>> {
             }
         }
 
-        // two, three, five
-        for &word in words.iter().filter(|w| w.len() == 5) {
-            let mut word: HashSet<_> = word.into_iter().copied().collect();
+        // two, three, five can be ignored
+        // for word in [five_1, five_2, five_3] {
+        //     let mut word: HashSet<_> = word.into_iter().copied().collect();
 
-            for n in [0, 1, 3, 4, 6] {
-                for byte in &wiring[n] {
-                    word.remove(byte);
-                }
-            }
+        //     for options in [&a, &b, &d, &e, &g] {
+        //         for byte in options {
+        //             word.remove(byte);
+        //         }
+        //     }
 
-            if !word.len() == 1 {
-                continue;
-            }
+        //     if !word.len() == 1 {
+        //         continue;
+        //     }
 
-            let remaining = word.into_iter().next().unwrap();
+        //     let remaining = word.into_iter().next().unwrap();
 
-            if wiring[2].contains(&remaining) {
-                wiring[2].retain(|&byte| byte == remaining);
-                wiring[5].retain(|&byte| byte != remaining);
-            }
-        }
+        //     if c.contains(&remaining) {
+        //         c.retain(|&byte| byte == remaining);
+        //         f.retain(|&byte| byte != remaining);
+        //     }
+        // }
 
-        let wiring = wiring.map(|set| set.into_iter().next().unwrap());
-        let [_, b, c, _, e, _, _] = wiring;
+        let b = b.into_iter().next().unwrap();
+        let c = c.into_iter().next().unwrap();
+        let e = e.into_iter().next().unwrap();
 
         let mut n = 0;
 
@@ -214,8 +205,11 @@ fn run() -> Result<(), Box<dyn Error>> {
         line.clear();
     }
 
-    println!("Part 1: {}, {:?}", p1, start.elapsed()); //
-    println!("Part 2: {}, {:?}", p2, start.elapsed()); //
+    println!("Part 1: {}, {:?}", p1, start.elapsed()); // 1.5ms
+    println!("Part 2: {}, {:?}", p2, start.elapsed()); // 1.94ms
+
+    assert_eq!(p1, 409);
+    assert_eq!(p2, 1_024_649);
 
     Ok(())
 }
