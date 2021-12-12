90e28f4265ffa838f944ae35b3baf67216fbc1d0
MaxOhn <ohn.m@hotmail.de>
2021-12-12 14:05:51 +0100

    day12 big improvement through preprocessing

--- 2021/ferris/src/day12/part2.rs
+++ 2021/ferris/src/day12/part2.rs
@@ -4,19 +4,17 @@ pub fn run(input: &[u8]) -> i64 {
     let (map, start, end) = parse_input(input);
 
     let mut paths = 0;
-    let mut stack = vec![(start, vec![start])];
+    let mut stack = vec![(start, vec![start], 1)];
     let mut buf = HashMap::new();
 
-    while let Some((cave, path)) = stack.pop() {
-        for &cave in map.get(cave) {
+    while let Some((cave, path, factor)) = stack.pop() {
+        for &(cave, count) in map.get(cave) {
             if cave == end {
-                paths += 1;
-            } else if !cave.is_small() {
-                stack.push((cave, path.clone()));
+                paths += factor * count;
             } else if is_valid(&path, cave, &mut buf) {
                 let mut path_ = path.clone();
                 path_.push(cave);
-                stack.push((cave, path_));
+                stack.push((cave, path_, factor * count));
             }
         }
     }
@@ -42,7 +40,7 @@ impl Id {
 }
 
 // Assuming there are no more than 20 caves
-struct Map([Vec<Id>; 20]);
+struct Map([Vec<(Id, i64)>; 20]);
 
 impl Map {
     fn new() -> Self {
@@ -72,11 +70,11 @@ impl Map {
         Self(arr)
     }
 
-    fn get(&self, id: Id) -> &[Id] {
+    fn get(&self, id: Id) -> &[(Id, i64)] {
         unsafe { self.0.get_unchecked((id.0 & !Id::SMALL) as usize) }
     }
 
-    fn get_mut(&mut self, id: Id) -> &mut Vec<Id> {
+    fn get_mut(&mut self, id: Id) -> &mut Vec<(Id, i64)> {
         unsafe { self.0.get_unchecked_mut((id.0 & !Id::SMALL) as usize) }
     }
 }
@@ -86,39 +84,91 @@ fn parse_input(input: &[u8]) -> (Map, Id, Id) {
 
     let mut ids = HashMap::new();
     let mut curr_id = 0;
-
-    let mut get_id = |s: &str| match ids.get(s) {
-        Some(id) => *id,
-        None => {
-            let id = Id::new(curr_id, s);
-            ids.insert(s.to_owned(), id);
-            curr_id += 1;
-
-            id
-        }
-    };
-
     let mut map = Map::new();
 
     for line in input.lines() {
         let (left, right) = line.split_once('-').unwrap();
 
-        let left = get_id(left);
-        let right = get_id(right);
+        let left = match ids.get(left) {
+            Some(id) => *id,
+            None => {
+                let id = Id::new(curr_id, left);
+                ids.insert(left, id);
+                curr_id += 1;
 
-        map.get_mut(left).push(right);
-        map.get_mut(right).push(left);
+                id
+            }
+        };
+
+        let right = match ids.get(right) {
+            Some(id) => *id,
+            None => {
+                let id = Id::new(curr_id, right);
+                ids.insert(right, id);
+                curr_id += 1;
+
+                id
+            }
+        };
+
+        map.get_mut(left).push((right, 1));
+        map.get_mut(right).push((left, 1));
     }
 
     let start = *ids.get("start").unwrap();
 
     for values in &mut map.0 {
-        values.retain(|&value| value != start);
+        values.retain(|(value, _)| *value != start);
     }
 
     let end = *ids.get("end").unwrap();
 
+    // Remove big caves and connect small caves directly instead
+    for from in (0..map.0.len() as u8).map(Id) {
+        for (cave, _) in map.get(from).to_vec() {
+            if !cave.is_small() {
+                // Remove path to big cave
+                {
+                    let reachable = map.get_mut(from);
+                    let idx = reachable.iter().position(|(id, _)| *id == cave).unwrap();
+                    reachable.remove(idx);
+                }
+
+                // Add paths to everything that was reachable from the big cave
+                for to in map.get(cave).iter().map(|(id, _)| *id).collect::<Vec<_>>() {
+                    extend(&mut map, from, to);
+                }
+            }
+        }
+    }
+
+    // The paths currently contain duplicates.
+    // Filter out those duplicates and use the count element instead.
+    for id in (0..map.0.len() as u8).map(Id) {
+        let mut reduced = Vec::new();
+
+        while let Some((curr, _)) = map.get_mut(id).pop() {
+            let count = map.get(id).iter().filter(|(id, _)| *id == curr).count();
+            reduced.push((curr, count as i64 + 1));
+            map.get_mut(id).retain(|(id, _)| *id != curr);
+        }
+
+        map.0[id.0 as usize] = reduced;
+    }
+
     (map, start, end)
+}
+
+// Add path in map between two caves.
+// If the destination is a big cave, add paths to its reachable caves instead.
+fn extend(map: &mut Map, from: Id, to: Id) {
+    if to.is_small() {
+        map.get_mut(from).push((to, 1));
+    } else {
+        for cave in map.get(to).iter().map(|(id, _)| *id).collect::<Vec<_>>() {
+            extend(map, from, cave);
+        }
+    }
 }
 
 #[inline(always)]
