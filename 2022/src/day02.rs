0469b456709f030a8bba7e8e4874c3fabfa8c334
MaxOhn <ohn.m@hotmail.de>
2022-12-02 10:13:21 +0100

    simplify day02

--- 2022/src/day02.rs
+++ 2022/src/day02.rs
@@ -3,64 +3,62 @@ use aoc22::prelude::Solution;
 pub fn run(input: &[u8]) -> Solution {
     let p1 = input
         .chunks(4)
-        .fold(Results::default(), |results, chunk| match &chunk[..3] {
-            [b'A', _, b'X'] => results.draw(1),
-            [b'A', _, b'Y'] => results.win(2),
-            [b'A', _, b'Z'] => results.loss(3),
-            [b'B', _, b'X'] => results.loss(1),
-            [b'B', _, b'Y'] => results.draw(2),
-            [b'B', _, b'Z'] => results.win(3),
-            [b'C', _, b'X'] => results.win(1),
-            [b'C', _, b'Y'] => results.loss(2),
-            [b'C', _, b'Z'] => results.draw(3),
-            _ => unreachable!("{:?}", chunk),
+        .fold(RockPaperScissors::default(), |rps, chunk| {
+            match &chunk[..3] {
+                [b'A', _, b'X'] => rps.draw(1),
+                [b'A', _, b'Y'] => rps.win(2),
+                [b'A', _, b'Z'] => rps.loss(3),
+                [b'B', _, b'X'] => rps.loss(1),
+                [b'B', _, b'Y'] => rps.draw(2),
+                [b'B', _, b'Z'] => rps.win(3),
+                [b'C', _, b'X'] => rps.win(1),
+                [b'C', _, b'Y'] => rps.loss(2),
+                [b'C', _, b'Z'] => rps.draw(3),
+                _ => unreachable!("{:?}", chunk),
+            }
         });
 
     let p2 = input
         .chunks(4)
-        .fold(Results::default(), |results, chunk| match &chunk[..3] {
-            [b'A', _, b'X'] => results.loss(3),
-            [b'A', _, b'Y'] => results.draw(1),
-            [b'A', _, b'Z'] => results.win(2),
-            [b'B', _, b'X'] => results.loss(1),
-            [b'B', _, b'Y'] => results.draw(2),
-            [b'B', _, b'Z'] => results.win(3),
-            [b'C', _, b'X'] => results.loss(2),
-            [b'C', _, b'Y'] => results.draw(3),
-            [b'C', _, b'Z'] => results.win(1),
-            _ => unreachable!("{:?}", chunk),
+        .fold(RockPaperScissors::default(), |rps, chunk| {
+            match &chunk[..3] {
+                [b'A', _, b'X'] => rps.loss(3),
+                [b'A', _, b'Y'] => rps.draw(1),
+                [b'A', _, b'Z'] => rps.win(2),
+                [b'B', _, b'X'] => rps.loss(1),
+                [b'B', _, b'Y'] => rps.draw(2),
+                [b'B', _, b'Z'] => rps.win(3),
+                [b'C', _, b'X'] => rps.loss(2),
+                [b'C', _, b'Y'] => rps.draw(3),
+                [b'C', _, b'Z'] => rps.win(1),
+                _ => unreachable!("{:?}", chunk),
+            }
         });
 
-    Solution::new().part1(p1.sum()).part2(p2.sum())
+    Solution::new().part1(p1.score).part2(p2.score)
 }
 
 #[derive(Default)]
-struct Results {
-    wins: u16,
-    draws: u16,
-    losses: u16,
+struct RockPaperScissors {
+    score: u16,
 }
 
-impl Results {
+impl RockPaperScissors {
     fn win(mut self, shape: u16) -> Self {
-        self.wins += shape + 6;
+        self.score += shape + 6;
 
         self
     }
 
     fn draw(mut self, shape: u16) -> Self {
-        self.draws += shape + 3;
+        self.score += shape + 3;
 
         self
     }
 
     fn loss(mut self, shape: u16) -> Self {
-        self.losses += shape;
+        self.score += shape;
 
         self
-    }
-
-    fn sum(self) -> u16 {
-        self.wins + self.draws + self.losses
     }
 }
