943042c260c7e66097e1b6b975f61c4a4196ec52
MaxOhn <ohn.m@hotmail.de>
2022-12-04 13:57:08 +0100

    use wee_alloc

--- site/src/lib.rs
+++ site/src/lib.rs
@@ -2,4 +2,9 @@ mod app;
 mod components;
 mod pages;
 
+use wee_alloc::WeeAlloc;
+
 pub use self::app::App;
+
+#[global_allocator]
+static ALLOC: WeeAlloc = WeeAlloc::INIT;
--- site/src/main.rs
+++ site/src/main.rs
@@ -1,8 +1,9 @@
+use aoc_site::App;
 use log::Level;
 use wasm_logger::Config;
 use yew::Renderer;
 
 fn main() {
     wasm_logger::init(Config::new(Level::Trace));
-    Renderer::<aoc_site::App>::new().render();
+    Renderer::<App>::new().render();
 }
