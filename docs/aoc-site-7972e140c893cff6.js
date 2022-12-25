f844e383806e2b5b1582862b915204127b4d83cf
MaxOhn <ohn.m@hotmail.de>
2022-12-25 08:58:35 +0100

    day25 for site

--- docs/aoc-site-7972e140c893cff6.js
+++ docs/aoc-site-7972e140c893cff6.js
@@ -835,16 +835,16 @@ function getImports() {
         getInt32Memory0()[arg0 / 4 + 1] = len0;
         getInt32Memory0()[arg0 / 4 + 0] = ptr0;
     };
-    imports.wbg.__wbindgen_closure_wrapper742 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 80, __wbg_adapter_34);
+    imports.wbg.__wbindgen_closure_wrapper744 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 81, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1039 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 80, __wbg_adapter_34);
+    imports.wbg.__wbindgen_closure_wrapper1041 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 81, __wbg_adapter_34);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1672 = function(arg0, arg1, arg2) {
-        const ret = makeMutClosure(arg0, arg1, 80, __wbg_adapter_39);
+    imports.wbg.__wbindgen_closure_wrapper1674 = function(arg0, arg1, arg2) {
+        const ret = makeMutClosure(arg0, arg1, 81, __wbg_adapter_39);
         return addHeapObject(ret);
     };
 
@@ -883,7 +883,7 @@ function initSync(module) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('aoc-site-12d4aa11b8957e91_bg.wasm', import.meta.url);
+        input = new URL('aoc-site-7972e140c893cff6_bg.wasm', import.meta.url);
     }
     const imports = getImports();
 
