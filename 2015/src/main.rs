666063bc798b9d21b2a49747fe2e6ded0da01b31
MaxOhn <ohn.m@hotmail.de>
2023-11-29 22:31:12 +0100

    restructure 2015

--- 2015/src/main.rs
+++ 2015/src/main.rs
@@ -1,10 +1,10 @@
 use std::{fs::read_to_string, time::Instant};
 
 fn main() {
-    let input = read_to_string("./input.txt").unwrap();
+    let input = read_to_string("./inputs/day07.txt").unwrap();
 
     let start = Instant::now();
-    let solution = aoc15_day07::run(&input);
+    let solution = aoc15::day07::run(&input);
     let elapsed = start.elapsed();
 
     print!("{}", solution.unwrap());
