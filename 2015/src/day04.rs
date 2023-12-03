ba26c646b37d31eda10dde7f7b18a494923ebda7
MaxOhn <ohn.m@hotmail.de>
2023-12-03 03:37:02 +0100

    optimize p2 by leveraging p1

--- 2015/src/day04.rs
+++ 2015/src/day04.rs
@@ -7,24 +7,24 @@ pub fn run(input: &str) -> Result<Solution> {
     let input = input.trim();
 
     let p1 = part1(input);
-    let p2 = part2(input);
+    let p2 = part2(input, p1);
 
     Ok(Solution::new().part1(p1).part2(p2))
 }
 
 fn part1(input: &str) -> u32 {
-    solve(input, |hash| hash.starts_with(&[0, 0]) && hash[2] < 8)
+    solve(input, 1, |hash| hash.starts_with(&[0, 0]) && hash[2] < 8)
 }
 
-fn part2(input: &str) -> u32 {
-    solve(input, |hash| hash.starts_with(&[0, 0, 0]))
+fn part2(input: &str, part1: u32) -> u32 {
+    solve(input, part1, |hash| hash.starts_with(&[0, 0, 0]))
 }
 
-fn solve<F: Fn(&Digest) -> bool>(input: &str, find: F) -> u32 {
+fn solve<F: Fn(&Digest) -> bool>(input: &str, start: u32, find: F) -> u32 {
     let mut int = Buffer::new();
     let mut buf = String::new();
 
-    (1..)
+    (start..)
         .find(|&n| {
             buf.clear();
             buf.push_str(input);
