a0033652c2f95925af6e9777b5f74565caf839c3
MaxOhn <ohn.m@hotmail.de>
2023-12-04 07:29:25 +0100

    optimize part 2

--- 2023/src/day04.rs
+++ 2023/src/day04.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 use aoc_rust::Solution;
 use eyre::{ContextCompat, Result, WrapErr};
 
@@ -16,90 +14,51 @@ fn part1(input: &str) -> Result<u32> {
     let mut sum = 0;
 
     for line in input.lines() {
-        let (_, suffix) = line.split_once(":").wrap_err("missing colon")?;
-        let (winning, owned) = suffix.split_once(" | ").wrap_err("missing split")?;
-
-        let winning = winning
-            .split(' ')
-            .filter(|n| !n.is_empty())
-            .map(str::parse)
-            .collect::<Result<Vec<u8>, _>>()
-            .wrap_err("invalid winning numbers")?;
-
-        let owned = owned
-            .split(' ')
-            .filter(|n| !n.is_empty())
-            .map(str::parse::<u8>);
-
-        let mut matches = 0;
-
-        for n in owned {
-            matches += winning.contains(&n?) as u8;
+        if let Some(matches) = parse_matches(line)?.checked_sub(1) {
+            sum += 1 << matches;
         }
-
-        sum += matches.checked_sub(1).map_or(0, |n| 1 << n);
     }
 
     Ok(sum)
 }
 
 fn part2(input: &str) -> Result<usize> {
-    let original = input
+    let matches = input
         .lines()
-        .map(Card::parse)
+        .map(parse_matches)
         .collect::<Result<Vec<_>>>()
         .wrap_err("invalid cards")?;
 
-    let mut cards_left = original.iter().map(|(id, _)| *id).collect::<VecDeque<_>>();
+    let mut counts = vec![1; matches.len()];
 
-    let mut total = cards_left.len();
-
-    while let Some(Id(id)) = cards_left.pop_front() {
-        let (idx, (_, Matches(matches))) = original
-            .iter()
-            .enumerate()
-            .find(|(_, (Id(card), _))| *card == id)
-            .unwrap();
-
-        for (id, _) in original[idx + 1..][..*matches as usize].iter() {
-            cards_left.push_back(*id);
-            total += 1;
+    for (i, matches) in matches.iter().enumerate() {
+        for j in 1..=*matches as usize {
+            counts[i + j] += counts[i];
         }
     }
 
-    Ok(total)
+    Ok(counts.iter().sum())
 }
 
-struct Card;
+fn parse_matches(line: &str) -> Result<u8> {
+    let line = line.trim_start_matches("Card").trim_start();
+    let (_, suffix) = line.split_once(":").wrap_err("missing colon")?;
+    let (winning, owned) = suffix.split_once(" | ").wrap_err("missing split")?;
 
-#[derive(Copy, Clone)]
-struct Id(u16);
+    let winning = winning
+        .split(' ')
+        .filter(|n| !n.is_empty())
+        .map(str::parse)
+        .collect::<Result<Vec<u8>, _>>()
+        .wrap_err("invalid winning numbers")?;
 
-struct Matches(u8);
+    let owned = owned.split(' ').filter(|n| !n.is_empty()).map(str::parse);
 
-impl Card {
-    fn parse(line: &str) -> Result<(Id, Matches)> {
-        let line = line.trim_start_matches("Card").trim_start();
-        let (id, suffix) = line.split_once(":").wrap_err("missing colon")?;
-        let (winning, owned) = suffix.split_once(" | ").wrap_err("missing split")?;
+    let mut matches = 0;
 
-        let id = id.parse().map_err(|_| eyre!("invalid id"))?;
-
-        let winning = winning
-            .split(' ')
-            .filter(|n| !n.is_empty())
-            .map(str::parse)
-            .collect::<Result<Vec<u8>, _>>()
-            .wrap_err("invalid winning numbers")?;
-
-        let owned = owned.split(' ').filter(|n| !n.is_empty()).map(str::parse);
-
-        let mut matches = 0;
-
-        for n in owned {
-            matches += winning.contains(&n?) as u8;
-        }
-
-        Ok((Id(id), Matches(matches)))
+    for n in owned {
+        matches += winning.contains(&n?) as u8;
     }
+
+    Ok(matches)
 }
