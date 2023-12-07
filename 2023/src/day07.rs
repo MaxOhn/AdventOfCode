edeaa2e8e89636caeb590da8b39f2e31f5d29368
MaxOhn <ohn.m@hotmail.de>
2023-12-07 12:39:45 +0100

    optimized day07

--- 2023/src/day07.rs
+++ 2023/src/day07.rs
@@ -1,52 +1,145 @@
-use std::{cmp::Ordering, str::FromStr};
+use std::{cmp::Ordering, marker::PhantomData, str::FromStr};
 
 use aoc_rust::Solution;
 use eyre::{ContextCompat, Report, Result, WrapErr};
 
 pub fn run(input: &str) -> Result<Solution> {
-    let hands = parse_input(input.trim())?;
+    let input = input.trim();
 
-    let p1 = part1(hands.clone());
-    let p2 = part2(hands);
+    let p1 = part1(input)?;
+    let p2 = part2(input)?;
 
     Ok(Solution::new().part1(p1).part2(p2))
 }
 
-fn parse_input(input: &str) -> Result<Vec<(Hand, u32)>> {
-    let mut hands = Vec::new();
-
-    for line in input.lines() {
-        let (hand, value) = line.split_once(' ').wrap_err("invalid line")?;
-        let hand: Hand = hand.parse()?;
-        let bid: u32 = value.parse().wrap_err("invalid bid")?;
-
-        hands.push((hand, bid));
-    }
-
-    Ok(hands)
+fn part1(input: &str) -> Result<u32> {
+    solve::<Part1>(input)
 }
 
-fn part1(mut hands: Vec<(Hand, u32)>) -> u32 {
-    sort_by_type(&mut hands, Hand::get_type, Card::cmp);
-
-    sum_values(&hands)
+fn part2(input: &str) -> Result<u32> {
+    solve::<Part2>(input)
 }
 
-fn part2(mut hands: Vec<(Hand, u32)>) -> u32 {
-    sort_by_type(&mut hands, Hand::get_type_with_joker, Card::cmp_joker);
-
-    sum_values(&hands)
-}
-
-fn sort_by_type<T, C>(hands: &mut [(Hand, u32)], get_type: T, cmp: C)
+fn solve<Part>(input: &str) -> Result<u32>
 where
-    T: Fn(Hand) -> Type,
-    C: Fn(&Card, &Card) -> Ordering,
+    Hand<Part>: FromStr<Err = Report> + Ord,
 {
-    hands.sort_unstable_by(|(a, _), (b, _)| {
-        get_type(*a).cmp(&get_type(*b)).then_with(|| {
-            for (a, b) in a.cards.iter().zip(b.cards.iter()) {
-                match cmp(a, b) {
+    let mut hands = input
+        .lines()
+        .map(str::parse)
+        .collect::<Result<Vec<Hand<Part>>>>()
+        .wrap_err("invalid hand")?;
+
+    hands.sort_unstable();
+
+    let winnings = hands
+        .into_iter()
+        .zip(1..)
+        .fold(0, |sum, (hand, rank)| sum + hand.bid * rank);
+
+    Ok(winnings)
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+struct Hand<Part> {
+    combo: Combo,
+    cards: [Card<Part>; 5],
+    bid: u32,
+}
+
+trait HandExt<Part> {
+    fn combo(cards: [Card<Part>; 5]) -> Combo;
+}
+
+impl HandExt<Part1> for Hand<Part1> {
+    fn combo(cards: [Card<Part1>; 5]) -> Combo {
+        let mut counts = [0_u8; 15];
+
+        for card in cards {
+            counts[card.0 as usize] += 1;
+        }
+
+        let (_, second, [first]) = counts.select_nth_unstable(13) else {
+            unreachable!()
+        };
+
+        match (*first, *second) {
+            (5, _) => Combo::FiveOfAKind,
+            (4, _) => Combo::FourOfAKind,
+            (3, 2) => Combo::FullHouse,
+            (3, _) => Combo::ThreeOfAKind,
+            (2, 2) => Combo::TwoPair,
+            (2, _) => Combo::OnePair,
+            _ => Combo::HighCard,
+        }
+    }
+}
+
+impl HandExt<Part2> for Hand<Part2> {
+    fn combo(cards: [Card<Part2>; 5]) -> Combo {
+        let mut counts = [0_u8; 15];
+        let mut jokers = 0;
+
+        for card in cards {
+            if card.is_joker() {
+                jokers += 1;
+            } else {
+                counts[card.0 as usize] += 1;
+            }
+        }
+
+        let (_, second, [first]) = counts.select_nth_unstable(13) else {
+            unreachable!()
+        };
+
+        match (*first + jokers, *second) {
+            (5, _) => Combo::FiveOfAKind,
+            (4, _) => Combo::FourOfAKind,
+            (3, 2) => Combo::FullHouse,
+            (3, _) => Combo::ThreeOfAKind,
+            (2, 2) => Combo::TwoPair,
+            (2, _) => Combo::OnePair,
+            _ => Combo::HighCard,
+        }
+    }
+}
+
+impl<Part> FromStr for Hand<Part>
+where
+    Hand<Part>: HandExt<Part>,
+    Card<Part>: Copy + TryFrom<u8, Error = Report>,
+{
+    type Err = Report;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let mut bytes = line.bytes();
+
+        let cards = [
+            Card::try_from(bytes.next().wrap_err("missing card")?)?,
+            Card::try_from(bytes.next().wrap_err("missing card")?)?,
+            Card::try_from(bytes.next().wrap_err("missing card")?)?,
+            Card::try_from(bytes.next().wrap_err("missing card")?)?,
+            Card::try_from(bytes.next().wrap_err("missing card")?)?,
+        ];
+
+        ensure!(bytes.next() == Some(b' '), "missing whitespace");
+
+        let bid = bytes.try_fold(0, |num, byte| match byte {
+            b'0'..=b'9' => Ok(num * 10 + (byte & 0xF) as u32),
+            _ => Err(eyre::eyre!("invalid digit byte `{byte}`")),
+        })?;
+
+        let combo = Self::combo(cards);
+
+        Ok(Self { combo, cards, bid })
+    }
+}
+
+impl Ord for Hand<Part1> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.combo.cmp(&other.combo).then_with(|| {
+            for (b, a) in other.cards.iter().zip(self.cards) {
+                match a.cmp(b) {
                     Ordering::Equal => {}
                     other => return other,
                 }
@@ -54,68 +147,95 @@ where
 
             Ordering::Equal
         })
-    });
+    }
 }
 
-fn sum_values(hands: &[(Hand, u32)]) -> u32 {
-    hands
-        .into_iter()
-        .zip(1..)
-        .map(|((_, bid), rank)| bid * rank)
-        .sum()
+impl PartialOrd for Hand<Part1> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Hand<Part2> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.combo.cmp(&other.combo).then_with(|| {
+            for (b, a) in other.cards.iter().zip(self.cards) {
+                match a.cmp(b) {
+                    Ordering::Equal => {}
+                    other => return other,
+                }
+            }
+
+            Ordering::Equal
+        })
+    }
+}
+
+impl PartialOrd for Hand<Part2> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-enum Card {
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Joker,
-    Queen,
-    King,
-    Ace,
+struct Card<Part>(u8, PhantomData<Part>);
+
+impl<Part> std::fmt::Debug for Card<Part> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Debug::fmt(&self.0, f)
+    }
 }
 
-impl Card {
-    fn cmp_joker(&self, other: &Self) -> Ordering {
-        match (self, other) {
-            (Card::Joker, Card::Joker) => Ordering::Equal,
-            (Card::Joker, _) => Ordering::Less,
-            (_, Card::Joker) => Ordering::Greater,
-            (a, b) => a.cmp(b),
-        }
+impl<Part> Card<Part> {
+    fn new(card: u8) -> Self {
+        Self(card, PhantomData)
     }
+}
 
-    fn from_byte(byte: u8) -> Result<Self> {
+impl TryFrom<u8> for Card<Part1> {
+    type Error = Report;
+
+    fn try_from(byte: u8) -> Result<Self, Self::Error> {
         let card = match byte {
-            b'2' => Card::Two,
-            b'3' => Card::Three,
-            b'4' => Card::Four,
-            b'5' => Card::Five,
-            b'6' => Card::Six,
-            b'7' => Card::Seven,
-            b'8' => Card::Eight,
-            b'9' => Card::Nine,
-            b'T' => Card::Ten,
-            b'J' => Card::Joker,
-            b'Q' => Card::Queen,
-            b'K' => Card::King,
-            b'A' => Card::Ace,
+            b'2'..=b'9' => byte - b'0',
+            b'T' => 10,
+            b'J' => 11,
+            b'Q' => 12,
+            b'K' => 13,
+            b'A' => 14,
             _ => eyre::bail!("invalid card byte `{byte}`"),
         };
 
-        Ok(card)
+        Ok(Self::new(card))
     }
 }
 
-#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
-enum Type {
+impl Card<Part2> {
+    fn is_joker(self) -> bool {
+        self.0 == 1
+    }
+}
+
+impl TryFrom<u8> for Card<Part2> {
+    type Error = Report;
+
+    fn try_from(byte: u8) -> Result<Self, Self::Error> {
+        let card = match byte {
+            b'2'..=b'9' => byte - b'0',
+            b'T' => 10,
+            b'J' => 1,
+            b'Q' => 12,
+            b'K' => 13,
+            b'A' => 14,
+            _ => eyre::bail!("invalid card byte `{byte}`"),
+        };
+
+        Ok(Self::new(card))
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+enum Combo {
     HighCard,
     OnePair,
     TwoPair,
@@ -125,235 +245,8 @@ enum Type {
     FiveOfAKind,
 }
 
-#[derive(Copy, Clone)]
-struct Hand {
-    cards: [Card; 5],
-}
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Part1;
 
-impl Hand {
-    fn get_type(self) -> Type {
-        if self.is_five_of_a_kind() {
-            Type::FiveOfAKind
-        } else if self.is_four_of_a_kind() {
-            Type::FourOfAKind
-        } else if self.is_full_house() {
-            Type::FullHouse
-        } else if self.is_three_of_a_kind() {
-            Type::ThreeOfAKind
-        } else if self.is_two_pair() {
-            Type::TwoPair
-        } else if self.is_one_pair() {
-            Type::OnePair
-        } else {
-            Type::HighCard
-        }
-    }
-
-    fn get_type_with_joker(self) -> Type {
-        if self.with_joker(Self::is_five_of_a_kind) {
-            Type::FiveOfAKind
-        } else if self.with_joker(Self::is_four_of_a_kind) {
-            Type::FourOfAKind
-        } else if self.with_joker(Self::is_full_house) {
-            Type::FullHouse
-        } else if self.with_joker(Self::is_three_of_a_kind) {
-            Type::ThreeOfAKind
-        } else if self.with_joker(Self::is_two_pair) {
-            Type::TwoPair
-        } else if self.with_joker(Self::is_one_pair) {
-            Type::OnePair
-        } else {
-            Type::HighCard
-        }
-    }
-
-    fn with_joker(self, f: impl Fn(Self) -> bool + Copy) -> bool {
-        const OPTIONS: [Card; 12] = [
-            Card::Two,
-            Card::Three,
-            Card::Four,
-            Card::Five,
-            Card::Six,
-            Card::Seven,
-            Card::Eight,
-            Card::Nine,
-            Card::Ten,
-            Card::Queen,
-            Card::King,
-            Card::Ace,
-        ];
-
-        for (i, card) in self.cards.into_iter().enumerate() {
-            if card != Card::Joker {
-                continue;
-            }
-
-            let mut new = self;
-
-            for option in OPTIONS {
-                new.cards[i] = option;
-
-                if new.with_joker(f) {
-                    return true;
-                }
-            }
-        }
-
-        f(self)
-    }
-
-    fn is_five_of_a_kind(self) -> bool {
-        for window in self.cards.windows(2) {
-            if window[0] != window[1] {
-                return false;
-            }
-        }
-
-        true
-    }
-
-    fn is_four_of_a_kind(self) -> bool {
-        let mut first = 0;
-        let mut second = 0;
-
-        for card in self.cards {
-            if card == self.cards[0] {
-                first += 1;
-            }
-
-            if card == self.cards[1] {
-                second += 1;
-            }
-        }
-
-        first == 4 || second == 4
-    }
-
-    fn is_full_house(self) -> bool {
-        let mut first = 0;
-        let mut second = 0;
-        let mut third = 0;
-        let mut fourth = 0;
-
-        for card in self.cards {
-            if card == self.cards[0] {
-                first += 1;
-            }
-
-            if card == self.cards[1] {
-                second += 1;
-            }
-
-            if card == self.cards[2] {
-                third += 1;
-            }
-
-            if card == self.cards[3] {
-                fourth += 1;
-            }
-        }
-
-        (first == 2 || second == 2 || third == 2 || fourth == 2)
-            && (first == 3 || second == 3 || third == 3 || fourth == 3)
-    }
-
-    fn is_three_of_a_kind(self) -> bool {
-        let mut first = 0;
-        let mut second = 0;
-        let mut third = 0;
-
-        for card in self.cards {
-            if card == self.cards[0] {
-                first += 1;
-            }
-
-            if card == self.cards[1] {
-                second += 1;
-            }
-
-            if card == self.cards[2] {
-                third += 1;
-            }
-        }
-
-        first == 3 || second == 3 || third == 3
-    }
-
-    fn is_two_pair(self) -> bool {
-        let mut first = 0;
-        let mut second = 0;
-        let mut third = 0;
-        let mut fourth = 0;
-
-        for card in self.cards {
-            if card == self.cards[0] {
-                first += 1;
-            }
-
-            if card == self.cards[1] {
-                second += 1;
-            }
-
-            if card == self.cards[2] {
-                third += 1;
-            }
-
-            if card == self.cards[3] {
-                fourth += 1;
-            }
-        }
-
-        let pairs = (first == 2) as usize
-            + (second == 2) as usize
-            + (third == 2) as usize
-            + (fourth == 2) as usize;
-
-        pairs >= 3
-    }
-
-    fn is_one_pair(self) -> bool {
-        let mut first = 0;
-        let mut second = 0;
-        let mut third = 0;
-        let mut fourth = 0;
-
-        for card in self.cards {
-            if card == self.cards[0] {
-                first += 1;
-            }
-
-            if card == self.cards[1] {
-                second += 1;
-            }
-
-            if card == self.cards[2] {
-                third += 1;
-            }
-
-            if card == self.cards[3] {
-                fourth += 1;
-            }
-        }
-
-        (first == 2 || second == 2 || third == 2 || fourth == 2)
-            && (first < 3 && second < 3 && third < 3 && fourth < 3)
-    }
-}
-
-impl FromStr for Hand {
-    type Err = Report;
-
-    fn from_str(hand: &str) -> Result<Self, Self::Err> {
-        let mut iter = hand.bytes().map(Card::from_byte);
-
-        let cards = [
-            iter.next().wrap_err("invalid hand")??,
-            iter.next().wrap_err("invalid hand")??,
-            iter.next().wrap_err("invalid hand")??,
-            iter.next().wrap_err("invalid hand")??,
-            iter.next().wrap_err("invalid hand")??,
-        ];
-
-        Ok(Self { cards })
-    }
-}
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct Part2;
