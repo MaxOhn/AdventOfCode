c90946f6dae215e8247e2a634f3254aef05cb9e2
MaxOhn <ohn.m@hotmail.de>
2023-12-08 09:37:23 +0100

    cleanup day08

--- 2023/src/day08.rs
+++ 2023/src/day08.rs
@@ -1,124 +1,111 @@
-use aoc_rust::Solution;
-use eyre::Result;
+use std::ops::ControlFlow;
+
+use aoc_rust::{util::numbers, Solution};
+use eyre::{ContextCompat, Result, WrapErr};
 
 pub fn run(input: &str) -> Result<Solution> {
-    let input = input.trim();
+    let mut lines = input.trim().lines();
 
-    let p1 = part1(input);
-    let p2 = part2(input);
+    let dirs = lines.next().wrap_err("missing instructions")?;
+
+    lines.next().wrap_err("missing blank line")?;
+
+    let mut instructions = lines
+        .map(Instruction::from_str)
+        .collect::<Result<Vec<_>>>()
+        .wrap_err("invalid instruction")?;
+
+    instructions.sort_unstable_by_key(|instr| instr.from);
+
+    let p1 = part1(dirs.bytes().cycle(), &instructions)?;
+    let p2 = part2(dirs.bytes().cycle(), &instructions)?;
 
     Ok(Solution::new().part1(p1).part2(p2))
 }
 
-fn part1(input: &str) -> u32 {
-    let mut lines = input.lines();
-
-    let dirs = lines.next().unwrap().bytes().cycle();
-
-    let _ = lines.next().unwrap();
-
-    let mut nodes = Vec::new();
-
-    for line in lines {
-        let (front, back) = line.split_once(" = ").unwrap();
-
-        let (left, right) = back
-            .trim_start_matches('(')
-            .trim_end_matches(')')
-            .split_once(", ")
-            .unwrap();
-
-        nodes.push((front, left, right));
-    }
-
-    let mut curr = "AAA";
-    let mut steps = 0;
-
-    for dir in dirs {
-        steps += 1;
-        let (_, left, right) = nodes.iter().find(|(from, ..)| *from == curr).unwrap();
-
-        match dir {
-            b'L' => curr = left,
-            b'R' => curr = right,
-            _ => panic!(),
+fn part1(dirs: impl Iterator<Item = u8>, instructions: &[Instruction]) -> Result<u32> {
+    let steps = dirs.zip(1..).try_fold("AAA", |mut curr, (dir, step)| {
+        if let Err(err) = process_curr(&mut curr, dir, instructions) {
+            return ControlFlow::Break(Err(err));
         }
 
-        if curr == "ZZZ" {
-            break;
+        if curr.ends_with('Z') {
+            ControlFlow::Break(Ok(step))
+        } else {
+            ControlFlow::Continue(curr)
         }
-    }
+    });
+
+    let ControlFlow::Break(steps) = steps else {
+        unreachable!()
+    };
 
     steps
 }
 
-fn part2(input: &str) -> u64 {
-    let mut lines = input.lines();
+fn part2(dirs: impl Iterator<Item = u8>, instructions: &[Instruction]) -> Result<u64> {
+    let mut currs: Vec<_> = instructions
+        .iter()
+        .filter_map(|instr| instr.from.ends_with('A').then_some(instr.from))
+        .collect();
 
-    let dirs = lines.next().unwrap().bytes().cycle();
+    let steps = dirs.zip(1..).try_fold(1, |mut steps, (dir, step)| {
+        currs.retain_mut(|curr| {
+            process_curr(curr, dir, instructions).unwrap();
 
-    let _ = lines.next().unwrap();
+            if curr.ends_with('Z') {
+                steps = numbers::lcm(steps, step);
 
-    let mut nodes = Vec::new();
+                false
+            } else {
+                true
+            }
+        });
 
-    for line in lines {
-        let (front, back) = line.split_once(" = ").unwrap();
+        if currs.is_empty() {
+            ControlFlow::Break(steps)
+        } else {
+            ControlFlow::Continue(steps)
+        }
+    });
+
+    let ControlFlow::Break(steps) = steps else {
+        unreachable!()
+    };
+
+    Ok(steps)
+}
+
+fn process_curr<'a>(curr: &mut &'a str, dir: u8, instructions: &[Instruction<'a>]) -> Result<()> {
+    let idx = instructions
+        .binary_search_by_key(curr, |instr| instr.from)
+        .map_err(|_| eyre::eyre!("missing instruction for `{curr}`"))?;
+
+    match dir {
+        b'L' => *curr = instructions[idx].left,
+        b'R' => *curr = instructions[idx].right,
+        _ => return Err(eyre::eyre!("invalid direction byte `{dir}`")),
+    }
+
+    Ok(())
+}
+
+struct Instruction<'a> {
+    from: &'a str,
+    left: &'a str,
+    right: &'a str,
+}
+
+impl<'a> Instruction<'a> {
+    fn from_str(line: &'a str) -> Result<Self> {
+        let (from, back) = line.split_once(" = ").wrap_err("missing equal")?;
 
         let (left, right) = back
             .trim_start_matches('(')
             .trim_end_matches(')')
             .split_once(", ")
-            .unwrap();
+            .wrap_err("missing comma")?;
 
-        nodes.push((front, left, right));
+        Ok(Self { from, left, right })
     }
-
-    nodes.sort_unstable_by_key(|(from, ..)| *from);
-
-    let mut currs: Vec<_> = nodes
-        .iter()
-        .filter_map(|(from, ..)| from.ends_with('A').then_some(*from))
-        .collect();
-
-    let mut until_z = vec![None; currs.len()];
-
-    let mut steps = 0;
-
-    for dir in dirs {
-        steps += 1;
-        let mut done = true;
-
-        for (curr, until_z) in currs.iter_mut().zip(until_z.iter_mut()) {
-            let idx = nodes
-                .binary_search_by_key(curr, |(from, ..)| *from)
-                .unwrap();
-
-            let (_, left, right) = &nodes[idx];
-
-            let next = match dir {
-                b'L' => left,
-                b'R' => right,
-                _ => panic!(),
-            };
-
-            *curr = next;
-
-            done &= if curr.ends_with('Z') {
-                let _ = until_z.get_or_insert(steps);
-
-                true
-            } else {
-                until_z.is_some()
-            }
-        }
-
-        if done {
-            break;
-        }
-    }
-
-    until_z
-        .into_iter()
-        .flatten()
-        .fold(1, |lcm, n| aoc_rust::util::numbers::lcm(lcm, n))
 }
