74a342fafeb04a32f9b66f7a0bddb19b5745ac04
MaxOhn <ohn.m@hotmail.de>
2023-12-15 06:49:33 +0100

    day15 cleanup

--- 2023/src/day15.rs
+++ 2023/src/day15.rs
@@ -1,71 +1,61 @@
-use std::collections::HashMap;
-
 use aoc_rust::Solution;
-use eyre::{Report, Result};
+use eyre::Result;
 
 pub fn run(input: &str) -> Result<Solution> {
     let input = input.trim();
 
-    let p1 = part1(input)?;
+    let p1 = part1(input);
     let p2 = part2(input)?;
 
     Ok(Solution::new().part1(p1).part2(p2))
 }
 
-fn part1(input: &str) -> Result<usize> {
+fn part1(input: &str) -> usize {
+    input.split(',').map(hash).sum()
+}
+
+fn part2(input: &str) -> Result<usize> {
+    let mut boxes = [0_u8; 256].map(|_| Vec::new());
+
+    for step in input.split(',') {
+        match step.split_once('=') {
+            Some((label, value)) => {
+                let value: u8 = value.parse().map_err(|_| eyre::eyre!("invalid value"))?;
+                let idx = hash(label);
+
+                if let Some((_, focal_len)) = boxes[idx].iter_mut().find(|(a, _)| *a == label) {
+                    *focal_len = value;
+                } else {
+                    boxes[idx].push((label, value));
+                }
+            }
+            None => {
+                let label = &step[..step.len() - 1];
+                let idx = hash(label);
+                boxes[idx].retain(|(a, _)| *a != label);
+            }
+        }
+    }
+
     let mut sum = 0;
 
-    for x in input.split(',') {
-        sum += hash(x);
+    for (b, i) in boxes.iter().zip(1..) {
+        for ((_, focal_len), j) in b.iter().zip(1..) {
+            sum += i * j * *focal_len as usize;
+        }
     }
 
     Ok(sum)
 }
 
 fn hash(s: &str) -> usize {
-    let mut curr = 0;
+    let mut hash = 0;
 
     for byte in s.bytes() {
-        curr += byte as usize;
-        curr *= 17;
-        curr %= 256;
+        hash += byte as usize;
+        hash *= 17;
+        hash %= 256;
     }
 
-    curr
-}
-
-fn part2<'a>(input: &'a str) -> Result<usize> {
-    let mut boxes = [0_u8; 256].map(|_| Vec::<(&str, usize)>::new());
-
-    for x in input.split(',') {
-        match x.split_once('=') {
-            Some((label, value)) => {
-                let h = hash(label);
-
-                if let Some((_, lense)) = boxes[h].iter_mut().find(|(a, _)| *a == label) {
-                    *lense = value.parse().unwrap();
-                } else {
-                    boxes[h].push((label, value.parse().unwrap()));
-                }
-            }
-            None => {
-                let label = &x[..x.len() - 1];
-                let h = hash(label);
-
-                if let Some(i) = boxes[h].iter().position(|(a, _)| *a == label) {
-                    boxes[h].remove(i);
-                }
-            }
-        }
-    }
-
-    let mut sum = 0;
-
-    for (i, b) in boxes.iter().enumerate() {
-        for (j, (_, f)) in b.iter().enumerate() {
-            sum += (1 + i) * (j + 1) * f;
-        }
-    }
-
-    Ok(sum)
+    hash
 }
