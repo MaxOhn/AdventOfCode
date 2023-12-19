08613cda68acb0b4dd6999df0aa63d93d8de09ad
MaxOhn <ohn.m@hotmail.de>
2023-12-19 11:27:49 +0100

    day19 cleanup

--- 2023/src/day19.rs
+++ 2023/src/day19.rs
@@ -1,32 +1,29 @@
-#![allow(unused)]
-
 use std::{
-    cmp,
-    collections::HashMap,
-    ops::{Index, IndexMut, Range, RangeInclusive},
+    ops::{Index, IndexMut, Range},
+    str::FromStr,
 };
 
 use aoc_rust::Solution;
-use eyre::Result;
+use eyre::{ContextCompat, Report, Result, WrapErr};
 
 pub fn run(input: &str) -> Result<Solution> {
-    let input = input.trim();
+    let (workflows, mut ratings) = parse_input(input.trim())?;
 
-    let p1 = part1(input);
-    let p2 = part2(input);
+    let p1 = part1(&workflows, &mut ratings)?;
+    let p2 = part2(&workflows)?;
 
     Ok(Solution::new().part1(p1).part2(p2))
 }
 
-fn part1(input: &str) -> i32 {
-    let (workflows, mut ratings) = parse_input(input);
+fn part1<'a>(workflows: &'a [WorkFlow<'a>], ratings: &mut [RatingSet<'a>]) -> Result<i32> {
+    let mut sum = 0;
 
     for set in ratings.iter_mut() {
         loop {
             let workflow = workflows
                 .iter()
                 .find(|workflow| workflow.name == set.workflow)
-                .unwrap_or_else(|| panic!("search for {}", set.workflow));
+                .wrap_err("missing workflow")?;
 
             let rule = workflow
                 .rules
@@ -36,54 +33,44 @@ fn part1(input: &str) -> i32 {
                         return true;
                     };
 
-                    let rating = match condition.op {
-                        Op::Greater => set.ratings.iter().find(|rating| {
-                            rating.name == condition.left && rating.value > condition.right
+                    match condition.op {
+                        Op::Greater => set.ratings.iter().any(|rating| {
+                            rating.part == condition.part && rating.value > condition.value
                         }),
-                        Op::Less => set.ratings.iter().find(|rating| {
-                            rating.name == condition.left && rating.value < condition.right
+                        Op::Less => set.ratings.iter().any(|rating| {
+                            rating.part == condition.part && rating.value < condition.value
                         }),
-                    };
-
-                    rating.is_some()
+                    }
                 })
-                .unwrap();
+                .wrap_err("missing rule")?;
 
             match rule.send_to {
-                "R" => set.workflow = "R",
-                "A" => set.workflow = "A",
+                "R" => break,
+                "A" => {
+                    for rating in set.ratings.iter() {
+                        sum += rating.value as i32;
+                    }
+
+                    break;
+                }
                 name => {
                     set.workflow = name;
                     continue;
                 }
             }
-
-            break;
         }
     }
 
-    let mut sum = 0;
-
-    for set in ratings {
-        if set.workflow == "A" {
-            for rating in set.ratings {
-                sum += rating.value as i32;
-            }
-        }
-    }
-
-    sum
+    Ok(sum)
 }
 
-fn part2(input: &str) -> u64 {
-    let (workflows, _) = parse_input(input);
-
-    let mut stack = vec![("in", 0, Status::default())];
+fn part2(workflows: &[WorkFlow]) -> Result<u64> {
+    let mut stack = vec![("in", 0, Ranges::default())];
     let mut accepted = 0;
 
-    while let Some((curr, idx, status)) = stack.pop() {
+    while let Some((curr, idx, ranges)) = stack.pop() {
         if curr == "A" {
-            accepted += status.value();
+            accepted += ranges.product();
             continue;
         } else if curr == "R" {
             continue;
@@ -92,73 +79,66 @@ fn part2(input: &str) -> u64 {
         let rule = workflows
             .iter()
             .find_map(|workflow| (workflow.name == curr).then(|| &workflow.rules[idx]))
-            .unwrap();
+            .wrap_err("missing workflow")?;
 
         let next = rule.send_to;
 
         let Some(ref condition) = rule.condition else {
-            stack.push((next, 0, status));
+            stack.push((next, 0, ranges));
             continue;
         };
 
-        let part = condition.left;
+        let part = condition.part;
 
         match condition.op {
             Op::Greater => {
-                if status[part].end <= condition.right {
-                    stack.push((curr, idx + 1, status));
-                } else if status[part].start > condition.right {
-                    stack.push((next, 0, status));
+                if ranges[part].end <= condition.value {
+                    stack.push((curr, idx + 1, ranges));
+                } else if ranges[part].start > condition.value {
+                    stack.push((next, 0, ranges));
                 } else {
-                    let mut else_status = status.clone();
-                    else_status[part].end = condition.right + 1;
-                    stack.push((curr, idx + 1, else_status));
+                    let mut else_ranges = ranges.clone();
+                    else_ranges[part].end = condition.value + 1;
+                    stack.push((curr, idx + 1, else_ranges));
 
-                    let mut match_status = status.clone();
-                    match_status[part].start = condition.right + 1;
-                    stack.push((next, 0, match_status));
+                    let mut match_ranges = ranges.clone();
+                    match_ranges[part].start = condition.value + 1;
+                    stack.push((next, 0, match_ranges));
                 }
             }
             Op::Less => {
-                if status[part].start >= condition.right {
-                    stack.push((curr, idx + 1, status));
-                } else if status[part].end < condition.right {
-                    stack.push((next, 0, status));
+                if ranges[part].start >= condition.value {
+                    stack.push((curr, idx + 1, ranges));
+                } else if ranges[part].end < condition.value {
+                    stack.push((next, 0, ranges));
                 } else {
-                    let mut else_status = status.clone();
-                    else_status[part].start = condition.right;
-                    stack.push((curr, idx + 1, else_status));
+                    let mut else_ranges = ranges.clone();
+                    else_ranges[part].start = condition.value;
+                    stack.push((curr, idx + 1, else_ranges));
 
-                    let mut match_status = status.clone();
-                    match_status[part].end = condition.right;
-                    stack.push((next, 0, match_status))
+                    let mut match_ranges = ranges.clone();
+                    match_ranges[part].end = condition.value;
+                    stack.push((next, 0, match_ranges))
                 }
             }
         }
     }
 
-    accepted
+    Ok(accepted)
 }
 
 #[derive(Clone, Debug)]
-struct Status {
+struct Ranges {
     parts: [Range<i16>; 4],
 }
 
-impl Status {
-    fn value(&self) -> u64 {
+impl Ranges {
+    fn product(&self) -> u64 {
         self.parts.iter().map(|range| range.len() as u64).product()
-    }
-
-    fn merge(&mut self, other: Self) {
-        for i in 0..4 {
-            self.parts[i] = self.parts[i].start.max(other.parts[i].start)
-                ..self.parts[i].end.min(other.parts[i].end);
-        }
     }
 }
 
-impl Index<Part> for Status {
+impl Index<Part> for Ranges {
     type Output = Range<i16>;
 
     fn index(&self, part: Part) -> &Self::Output {
@@ -166,13 +146,13 @@ impl Index<Part> for Status {
     }
 }
 
-impl IndexMut<Part> for Status {
+impl IndexMut<Part> for Ranges {
     fn index_mut(&mut self, part: Part) -> &mut Self::Output {
         self.parts.index_mut(part as usize)
     }
 }
 
-impl Default for Status {
+impl Default for Ranges {
     fn default() -> Self {
         Self {
             parts: [0_u8; 4].map(|_| 1..4001),
@@ -180,36 +160,41 @@ impl Default for Status {
     }
 }
 
-fn parse_input(input: &str) -> (Vec<WorkFlow>, Vec<RatingSet>) {
-    let (workflows, ratings) = input.split_once("\n\n").unwrap();
+fn parse_input(input: &str) -> Result<(Vec<WorkFlow>, Vec<RatingSet>)> {
+    let (workflows, ratings) = input
+        .split_once("\n\n")
+        .wrap_err("missing double newline")?;
 
     let workflows: Vec<_> = workflows
         .lines()
         .map(|line| {
-            let (name, rules) = line.trim_end_matches('}').split_once("{").unwrap();
+            let (name, rules) = line
+                .trim_end_matches('}')
+                .split_once("{")
+                .wrap_err("missing parentheses")?;
 
-            let rules: Vec<_> = rules
+            let rules = rules
                 .split(',')
                 .map(|rule| {
                     let Some((condition, send_to)) = rule.split_once(':') else {
-                        return Rule {
+                        return Ok(Rule {
                             condition: None,
                             send_to: rule,
-                        };
+                        });
                     };
 
-                    Rule {
-                        condition: Some(Condition::new(condition)),
+                    Ok(Rule {
+                        condition: Some(condition.parse()?),
                         send_to,
-                    }
+                    })
                 })
-                .collect();
+                .collect::<Result<Vec<_>>>()?;
 
-            WorkFlow { name, rules }
+            Ok(WorkFlow { name, rules })
         })
-        .collect();
+        .collect::<Result<_>>()?;
 
-    let ratings: Vec<_> = ratings
+    let ratings = ratings
         .lines()
         .map(|line| {
             let mut ratings = line
@@ -218,35 +203,45 @@ fn parse_input(input: &str) -> (Vec<WorkFlow>, Vec<RatingSet>) {
                 .split(',');
 
             let ratings = [
-                ratings.next().map(Rating::new).unwrap(),
-                ratings.next().map(Rating::new).unwrap(),
-                ratings.next().map(Rating::new).unwrap(),
-                ratings.next().map(Rating::new).unwrap(),
+                ratings
+                    .next()
+                    .map(Rating::from_str)
+                    .wrap_err("missing rating")??,
+                ratings
+                    .next()
+                    .map(Rating::from_str)
+                    .wrap_err("missing rating")??,
+                ratings
+                    .next()
+                    .map(Rating::from_str)
+                    .wrap_err("missing rating")??,
+                ratings
+                    .next()
+                    .map(Rating::from_str)
+                    .wrap_err("missing rating")??,
             ];
 
-            RatingSet {
+            Ok(RatingSet {
                 workflow: "in",
                 ratings,
-            }
+            })
         })
-        .collect();
+        .collect::<Result<_>>()?;
 
-    (workflows, ratings)
+    Ok((workflows, ratings))
 }
 
-#[derive(Debug)]
 struct RatingSet<'a> {
     workflow: &'a str,
     ratings: [Rating; 4],
 }
 
-#[derive(Debug)]
 struct Rating {
-    name: Part,
+    part: Part,
     value: i16,
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 enum Part {
     X,
     M,
@@ -254,73 +249,71 @@ enum Part {
     S,
 }
 
-impl Part {
-    fn new(s: &str) -> Self {
+impl FromStr for Part {
+    type Err = Report;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "x" => Self::X,
-            "m" => Self::M,
-            "a" => Self::A,
-            "s" => Self::S,
-            _ => unreachable!(),
+            "x" => Ok(Self::X),
+            "m" => Ok(Self::M),
+            "a" => Ok(Self::A),
+            "s" => Ok(Self::S),
+            _ => eyre::bail!("invalid part `{s}`"),
         }
     }
 }
 
-impl Rating {
-    fn new(s: &str) -> Self {
-        let (name, value) = s.split_once('=').unwrap();
+impl FromStr for Rating {
+    type Err = Report;
 
-        Self {
-            name: Part::new(name),
-            value: value.parse().unwrap(),
-        }
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (part, value) = s.split_once('=').wrap_err("missing equal")?;
+
+        Ok(Self {
+            part: part.parse()?,
+            value: value.parse().wrap_err("invalid value")?,
+        })
     }
 }
 
-#[derive(Debug)]
 struct WorkFlow<'a> {
     name: &'a str,
     rules: Vec<Rule<'a>>,
 }
 
-#[derive(Debug)]
 struct Rule<'a> {
     condition: Option<Condition>,
     send_to: &'a str,
 }
 
-#[derive(Debug)]
 struct Condition {
-    left: Part,
+    part: Part,
     op: Op,
-    right: i16,
+    value: i16,
 }
 
-impl Condition {
-    fn new(s: &str) -> Self {
-        if let Some(_) = s.find('>') {
-            let (name, value) = s.split_once('>').unwrap();
+impl FromStr for Condition {
+    type Err = Report;
 
-            Self {
-                left: Part::new(name),
-                op: Op::Greater,
-                right: value.parse().unwrap(),
-            }
-        } else if let Some(_) = s.find('<') {
-            let (name, value) = s.split_once('<').unwrap();
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (left, op, right) = match s.split_once('>') {
+            Some((left, right)) => (left, Op::Greater, right),
+            None => {
+                let (left, right) = s.split_once('<').wrap_err("missing op")?;
 
-            Self {
-                left: Part::new(name),
-                op: Op::Less,
-                right: value.parse().unwrap(),
+                (left, Op::Less, right)
             }
-        } else {
-            panic!()
-        }
+        };
+
+        Ok(Self {
+            part: left.parse()?,
+            op,
+            value: right.parse().wrap_err("invalid value")?,
+        })
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone)]
 enum Op {
     Greater,
     Less,
