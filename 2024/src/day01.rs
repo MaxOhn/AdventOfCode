0d74df32368eb5eecbdb3b9701d1a0199d268fa0
MaxOhn <ohn.m@hotmail.de>
2024-12-01 15:50:40 +0100

    prepare 2024

---
+++ 2024/src/day01.rs
@@ -0,0 +1,19 @@
+use aoc_rust::Solution;
+use eyre::Result;
+
+pub fn run(input: &str) -> Result<Solution> {
+    let input = input.trim();
+
+    let p1 = part1(input);
+    let p2 = part2(input);
+
+    Ok(Solution::new().part1(p1).part2(p2))
+}
+
+fn part1(input: &str) -> u32 {
+    todo!()
+}
+
+fn part2(input: &str) -> u32 {
+    todo!()
+}
---
+++ 2024/src/main.rs
@@ -0,0 +1,25 @@
+mod day01;
+
+use std::{fs, time::Instant};
+
+use eyre::Result;
+
+fn main() -> Result<()> {
+    macro_rules! load {
+        ( $day:ident ) => {
+            (concat!("./inputs/", stringify!($day), ".txt"), $day::run)
+        };
+    }
+
+    let (path, run) = load!(day01);
+    let input = fs::read_to_string(path)?;
+
+    let start = Instant::now();
+    let solution = run(&input)?;
+    let elapsed = start.elapsed();
+
+    println!("{solution}");
+    println!("Elapsed: {elapsed:?}");
+
+    Ok(())
+}
