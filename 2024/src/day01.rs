f14565f71ceafcae16dd79bdffd45597f3fb83c7
MaxOhn <ohn.m@hotmail.de>
2024-12-01 16:45:08 +0100

    day01

--- 2024/src/day01.rs
+++ 2024/src/day01.rs
@@ -10,10 +10,45 @@ pub fn run(input: &str) -> Result<Solution> {
     Ok(Solution::new().part1(p1).part2(p2))
 }
 
-fn part1(input: &str) -> u32 {
-    todo!()
+fn parse(input: &str) -> (Vec<i32>, Vec<i32>) {
+    input
+        .lines()
+        .map(|line| {
+            let mut iter = line
+                .split_ascii_whitespace()
+                .map(str::parse::<i32>)
+                .map(Result::unwrap);
+
+            (iter.next().unwrap(), iter.next().unwrap())
+        })
+        .unzip()
 }
 
-fn part2(input: &str) -> u32 {
-    todo!()
+fn part1(input: &str) -> i32 {
+    let (mut left, mut right) = parse(input);
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    left.into_iter()
+        .zip(right)
+        .fold(0, |sum, (left, right)| sum + (right - left).abs())
+}
+
+fn part2(input: &str) -> i32 {
+    let (left, mut right) = parse(input);
+
+    right.sort_unstable();
+
+    left.iter().fold(0, |sum, &left| {
+        let Ok(idx) = right.binary_search(&left) else {
+            return sum;
+        };
+
+        let (to_left, to_right) = right.split_at(idx);
+        let to_left = to_left.iter().rev().take_while(|&&n| n == left).count();
+        let to_right = to_right.iter().take_while(|&&n| n == left).count();
+
+        sum + left * (to_left + to_right) as i32
+    })
 }
