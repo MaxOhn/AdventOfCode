8b230325a76bb613c770d3dce3c73bdb152e04ad
MaxOhn <ohn.m@hotmail.de>
2024-12-05 07:47:27 +0100

    use hashmaps

--- 2024/src/day05.rs
+++ 2024/src/day05.rs
@@ -1,7 +1,8 @@
-use std::cell::RefCell;
+use std::{cell::RefCell, collections::HashMap};
 
 use aoc_rust::Solution;
 use eyre::Result;
+use fxhash::FxBuildHasher;
 use rayon::{iter::ParallelIterator, str::ParallelString};
 
 pub fn run(input: &str) -> Result<Solution> {
@@ -17,13 +18,19 @@ thread_local! {
     static UPDATES_BUF: RefCell<Vec<u8>> = RefCell::new(Vec::new());
 }
 
-fn parse_rules(rules: &str) -> Vec<Rule> {
-    rules
+fn parse_rules(rules: &str) -> HashMap<u8, Vec<u8>, FxBuildHasher> {
+    let iter = rules
         .lines()
         .filter_map(|line| line.split_once('|'))
-        .filter_map(|(x, y)| x.parse().ok().zip(y.parse().ok()))
-        .map(|(x, y)| Rule::new(x, y))
-        .collect()
+        .filter_map(|(x, y)| x.parse().ok().zip(y.parse().ok()));
+
+    let mut rules = HashMap::<u8, Vec<u8>, _>::with_hasher(FxBuildHasher::default());
+
+    for (x, y) in iter {
+        rules.entry(x).or_default().push(y);
+    }
+
+    rules
 }
 
 fn part1(input: &str) -> u16 {
@@ -44,10 +51,13 @@ fn part1(input: &str) -> u16 {
 
                 rules
                     .iter()
-                    .all(|rule| {
-                        memchr::memchr(rule.x, updates)
-                            .zip(memchr::memchr(rule.y, updates))
-                            .map_or(true, |(x, y)| x < y)
+                    .all(|(x, ys)| {
+                        let Some(ix) = memchr::memchr(*x, updates) else {
+                            return true;
+                        };
+
+                        ys.iter()
+                            .all(|&y| memchr::memchr(y, updates).map_or(true, |iy| ix < iy))
                     })
                     .then_some(updates[updates.len() / 2] as u16)
             })
@@ -78,14 +88,19 @@ fn part2(input: &str) -> u16 {
                     iters += 1;
                     sorted = true;
 
-                    for rule in rules.iter() {
-                        let x = memchr::memchr(rule.x, updates);
-                        let y = memchr::memchr(rule.y, updates);
+                    for (x, ys) in rules.iter() {
+                        let Some(ix) = memchr::memchr(*x, updates) else {
+                            continue;
+                        };
 
-                        if let (Some(x), Some(y)) = (x, y) {
-                            if y < x {
-                                sorted = false;
-                                updates.swap(x, y);
+                        for &y in ys {
+                            let iy = memchr::memchr(y, updates);
+
+                            if let Some(iy) = iy {
+                                if iy < ix {
+                                    sorted = false;
+                                    updates.swap(ix, iy);
+                                }
                             }
                         }
                     }
@@ -95,15 +110,4 @@ fn part2(input: &str) -> u16 {
             })
         })
         .sum()
-}
-
-struct Rule {
-    x: u8,
-    y: u8,
-}
-
-impl Rule {
-    fn new(x: u8, y: u8) -> Self {
-        Self { x, y }
-    }
 }
