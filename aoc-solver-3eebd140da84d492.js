0ca7df2667392b84be37aefb2096b0c3906532c3
MaxOhn <MaxOhn@users.noreply.github.com>
2024-12-14 11:19:42 +0000

    deploy: 0ed2508157a2fffc13bc16ad46a583c03978e989

--- aoc-solver-3eebd140da84d492.js
+++ aoc-solver-3eebd140da84d492.js
@@ -983,7 +983,7 @@ async function __wbg_init(module_or_path) {
     }
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('aoc-solver-11dc49dcea362a65_bg.wasm', import.meta.url);
+        module_or_path = new URL('aoc-solver-3eebd140da84d492_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
