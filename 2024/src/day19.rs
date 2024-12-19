52043f397254775b283b81e9ac895f2b234a3d1a
MaxOhn <ohn.m@hotmail.de>
2024-12-19 18:39:26 +0100

    impl trie

--- 2024/src/day19.rs
+++ 2024/src/day19.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, convert::identity};
+use std::{collections::HashMap, convert::identity, mem};
 
 use aoc_rust::Solution;
 use eyre::Result;
@@ -6,7 +6,7 @@ use nom::{
     bytes::complete as by,
     character::complete as ch,
     combinator::{flat_map, map, opt},
-    multi::{fold_many1, separated_list1},
+    multi::fold_many1,
     sequence::terminated,
 };
 
@@ -30,18 +30,23 @@ fn part2(input: &str) -> i64 {
 }
 
 fn solve<P: Part>(input: &str) -> P::Output {
-    let available = terminated(separated_list1(by::tag(", "), ch::alpha1), ch::multispace1);
+    let pattern = terminated(ch::alpha1, opt(by::tag(", ")));
+    let fold_trie = fold_many1(pattern, Trie::default, |mut trie, next| {
+        trie.insert(next);
 
-    let applied = |mut available: Vec<_>| {
-        available.sort_unstable(); // why does this improve performance
+        trie
+    });
+    let trie = terminated(fold_trie, ch::multispace1);
+
+    let applied = |trie| {
         let mut cache = Cache::<P>::default();
         let line = terminated(ch::alpha1, opt(ch::newline::<_, ()>));
-        let apply = move |line| P::recurse(&available, line, &mut cache);
+        let apply = move |line| P::recurse(&trie, line, &mut cache);
 
         fold_many1(map(line, apply), <P::Output>::default, P::fold_output)
     };
 
-    let (_, res) = flat_map(available, applied)(input).expect("bad input");
+    let (_, res) = flat_map(trie, applied)(input).expect("bad input");
 
     res
 }
@@ -56,19 +61,18 @@ trait Part {
 
     fn fold_output(acc: Self::Output, next: Self::Cached) -> Self::Output;
 
-    fn recurse<'a>(available: &[&str], find: &'a str, cache: &mut Cache<'a, Self>) -> Self::Cached {
-        if let Some(cached) = cache.get(find) {
+    fn recurse<'a>(trie: &Trie<'a>, rest: &'a str, cache: &mut Cache<'a, Self>) -> Self::Cached {
+        if let Some(cached) = cache.get(rest) {
             *cached
-        } else if find.is_empty() {
+        } else if rest.is_empty() {
             Self::EMPTY
         } else {
-            let iter = available
-                .iter()
-                .filter_map(|a| find.strip_prefix(a))
-                .map(|find| Self::recurse(available, find, cache));
+            let iter = trie
+                .common_prefix_search(rest)
+                .map(|len| Self::recurse(trie, &rest[len..], cache));
 
             let res = Self::fold_cached(iter);
-            cache.insert(find, res);
+            cache.insert(rest, res);
 
             res
         }
@@ -106,5 +110,119 @@ impl Part for Part2 {
 
     fn fold_output(acc: Self::Output, next: Self::Cached) -> Self::Output {
         acc + next
+    }
+}
+
+#[derive(Default)]
+pub struct Trie<'a> {
+    root: Node<'a>,
+}
+
+impl<'a> Trie<'a> {
+    pub fn insert(&mut self, value: &'a str) {
+        let mut curr = &mut self.root;
+        let mut rest = value;
+
+        'outer: loop {
+            for i in (0..curr.children.len()).rev() {
+                if rest == curr.children[i].value {
+                    curr.children[i].real = true;
+
+                    return;
+                } else if let Some(new_value) = curr.children[i].value.strip_prefix(rest) {
+                    let new_node = Node::new(rest, true);
+                    let mut old_node = mem::replace(&mut curr.children[i], new_node);
+                    old_node.value = new_value;
+                    curr.children[i].children.push(old_node);
+
+                    return;
+                } else if let Some(new_rest) = rest.strip_prefix(curr.children[i].value) {
+                    rest = new_rest;
+                    curr = &mut curr.children[i];
+
+                    continue 'outer;
+                }
+
+                let same_prefix_len = curr.children[i]
+                    .value
+                    .bytes()
+                    .zip(rest.bytes())
+                    .take_while(|(a, b)| a == b)
+                    .count();
+
+                if same_prefix_len > 0 {
+                    let (prefix, new_rest) = rest.split_at(same_prefix_len);
+                    let shared = Node::new(prefix, false);
+                    let new_node = Node::new(new_rest, true);
+                    let mut old_node = mem::replace(&mut curr.children[i], shared);
+                    old_node.value = &old_node.value[same_prefix_len..];
+                    curr.children[i].children.push(old_node);
+                    curr.children[i].children.push(new_node);
+
+                    return;
+                }
+            }
+
+            curr.children.push(Node::new(rest, true));
+
+            return;
+        }
+    }
+
+    pub fn common_prefix_search(&'a self, query: &'a str) -> CommonPrefixIter<'a> {
+        CommonPrefixIter::new(&self.root, query)
+    }
+}
+
+#[derive(Default)]
+struct Node<'a> {
+    value: &'a str,
+    real: bool,
+    children: Vec<Node<'a>>,
+}
+
+impl<'a> Node<'a> {
+    fn new(value: &'a str, real: bool) -> Self {
+        Node {
+            value,
+            real,
+            children: Vec::new(),
+        }
+    }
+}
+
+pub struct CommonPrefixIter<'a> {
+    curr: &'a Node<'a>,
+    rest: &'a str,
+    len: usize,
+}
+
+impl<'a> CommonPrefixIter<'a> {
+    fn new(curr: &'a Node<'a>, rest: &'a str) -> Self {
+        Self { curr, rest, len: 0 }
+    }
+}
+
+impl Iterator for CommonPrefixIter<'_> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        'outer: loop {
+            for child in self.curr.children.iter() {
+                if let Some(new_rest) = self.rest.strip_prefix(child.value) {
+                    self.len += child.value.len();
+                    self.curr = child;
+                    self.rest = new_rest;
+
+                    if child.real {
+                        return Some(self.len);
+                    } else {
+                        continue 'outer;
+                    }
+                }
+            }
+
+            return None;
+        }
     }
 }
